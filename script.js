7fb00d0993746c024fff575b2607c73d08bd626c
bozze007 <126372581+bozze007@users.noreply.github.com>
2023-04-02 11:12:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,44 @@
+// Wait for the page to load
 window.addEventListener("load", function() {
-  var loadingScreen = document.getElementById("loading-screen");
+
+  // Get the loading screen elements
+  var loadingScreen1 = document.getElementById("loading-screen");
+  var loadingScreen2 = document.getElementById("loading-screen-2");
+  
+  // Get the videos
+  var video1 = document.getElementById("loading-video-1");
+  var video2 = document.getElementById("loading-video-2");
+  
+  // Get the game screen element
   var gameScreen = document.getElementById("game-screen");
 
+  // Hide the second loading screen and the game screen
+  loadingScreen2.style.display = "none";
+  gameScreen.style.display = "none";
+
+  // When the first video ends, hide it and show the second video
+  video1.addEventListener("ended", function() {
+    video1.style.display = "none";
+    loadingScreen2.style.display = "block";
+    video2.play();
+  });
+
+  // When the second video ends, hide the loading screen and show the game screen
+  video2.addEventListener("ended", function() {
+    loadingScreen2.style.display = "none";
+    gameScreen.style.display = "block";
+  });
+
+  // Wait for 3 seconds and then hide the first loading screen and show the first video
   setTimeout(function() {
-    loadingScreen.style.opacity = 0;
-    setTimeout(function() {
-      loadingScreen.style.display = "none";
-      gameScreen.style.display = "block";
-      setTimeout(function() {
-        gameScreen.style.opacity = 1;
-      }, 100);
-    }, 500);
+    loadingScreen1.style.display = "none";
+    video1.play();
   }, 3000);
+  
 });
 
-//efek video loading screen
-var video1 = document.getElementById("loading-video-1");
-var video2 = document.getElementById("loading-video-2");
 
-video1.addEventListener("ended", function() {
-  video1.style.display = "none";
-  video2.style.display = "block";
-  video2.play();
-});
+
 
 //tampilan pilih char
 var characters = ['burung-removebg-preview.png', 'monyet-removebg-preview.png', 'panda-removebg-preview.png'];
@@ -68,9 +84,3 @@ function updatePreview() {
       }
       updatePreview();
       });
-
-video1.addEventListener("ended", function() {
-  video1.style.display = "none";
-  video2.style.display = "block";
-  video2.play();
-});
